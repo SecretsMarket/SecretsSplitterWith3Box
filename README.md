12472e6784e2279bc49d341387efd624132dd201
mikedeshazer <guyonaplane57@gmail.com>
2020-07-12 22:04:59 +0900

    simple implementation for splitting/uploading with restful api

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ## Simple Node.js RESTful API for splitting and storing data using 3Box
 
 Super, super light to just get started.
+
+### Getting started
+
+Run `npm install`
+
+In index.js, update the provider with your provider details (Infura API/key).
+
+Then `npm start`
+
+Navigate to your browser and visit: http://localhost:5000
+
+Generate An Ethereum Key pair to authenticate with 3Box with the endpoint: http://localhost:5000/ethcreate
+
+Upload text content and split it into 3 storage 3Box spaces sending the parameter of "content": http://localhost:5000/upload
+
+Give permission for a private  3box thread: http://localhost:5000/grantpermissions?adresses=["0x...dsdss", "INSERT YOUR ADDRESSES"]
+
+
+## TODO
+
+-Tests
+-Support Client-based signatures
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var bodyParser = require("body-parser");
 var cors = require('cors');
 
 var Wallet = require('ethereumjs-wallet');
-
+const Box = require('3box');
 
 
 var app = express();
@@ -28,6 +28,9 @@ var server = http.createServer(app).listen(PORT, () => console.log(`Listening on
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+//const Web3 = require('web3');
+
+//let web3 = new Web3('ws://localhost:8546');
 
 
 var cors = require('cors');
@@ -54,6 +57,34 @@ app.use(cors({credentials: true, origin: '*'}));
  })
 
 
+app.get("/upload", async function(req, res){
+
+
+  var contentNeedSplitting = req.query.content;
+  if(typeof req.query.content != "string"){
+    res.send({"status":"fail", "msg":"Please send content"});
+    return;
+  }
+  splitUp = split3Parts(contentNeedSplitting);
+
+
+  const cw3p = require('create-web3-provider');
+  let provider = cw3p();
+  //add your web3 provider/Infura info here
+  provider = cw3p({ws: true, network: 'ropsten'});
+  const box = await Box.create()
+  var addr =  Wallet.generate();
+  const address = addr.getAddressString()
+  const spaces = ['secretsDapp']
+
+  await box.auth(spaces, { address, provider })
+
+  await box.private.set('content1', splitUp[0])
+  await box.private.set('content2', splitUp[1])
+  await box.private.set('content3', splitUp[2])
+
+  res.send({"status":"success", "msg":"It worked!"});
+})
 
 
 
@@ -72,6 +103,18 @@ function makeETHAddress(respObj){
 
 }
 
+
+function split3Parts(theSubString){
+  var parts = theSubString.length;
+  var by3 = parseInt(parts/3);
+   regexA = new RegExp('.{1,'+by3+'}', "g");
+    theReturn = theSubString.match(regexA);
+    if(theReturn.length >3){
+      theReturn[2] = theReturn[2]+ theReturn[3];
+    }
+    theReturn.pop()
+    return theReturn;
+}
 
 
 
